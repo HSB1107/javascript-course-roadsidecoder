b53ce33f8ee9606980bad361545d7929b2fd68da
Hrishikesh Bakshi <bakshihrishikesh1107@gmail.com>
2026-02-28 21:16:48 +0530

    learn: add self-explanatory comments for the understanding of methods provided by React

--- ReactModule/Chapter 1.1 React Under the Hood/script.js
+++ ReactModule/Chapter 1.1 React Under the Hood/script.js
@@ -1,20 +1,31 @@
-function Counter(){
-    // return <div>Count: 0</div>
-    const [count, setCount] = React.useState(0);
+function Counter() {
+  // return <div>Count: 0</div>
+  const [count, setCount] = React.useState(0);
 
-    const increament = ()=>{
-        setCount(count + 1);
-    };
-    // return React.createElement("div", null, 
-    //     React.createElement("p", null, `Count: ${count}`),
-    //     React.createElement("button", {onClick: increament}, `Increament`)
-    // )
-    return (
-        <div>
-            <p>Count: {count}</p>
-            <button onClick={increament}>Increament</button>
-        </div>
-    )
+  const increament = () => {
+    setCount(count + 1);
+  };
+
+  /**
+   * createElement(type: "input",
+   * props?: (React.InputHTMLAttributes<HTMLInputElement> & React.ClassAttributes<HTMLInputElement>) | null,
+   * ...children: React.ReactNode[]): React.DetailedReactHTMLElement<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+   */
+
+  // return React.createElement("div", null,
+  //     React.createElement("p", null, `Count: ${count}`),
+  //     React.createElement("button", {onClick: increament}, `Increament`)
+  // )
+
+  /**
+   * This is achieve by the babel transpiler - the conversion of html like syntax into React.createElement tree
+   */
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increament}>Increament</button>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
